a38beae511fcef75cec4d6e53f8c1e53dcda4f14
allen-fang <allenfang08@gmail.com>
2018-01-11 11:44:34 -0800

    added routes for habitats

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const monsters = require('./routes/monsters');
+const habitats = require('./routes/habitats');
 
 const app = express();
 
 
 // middlewares
+app.use(bodyParser.json());
 app.use('/monsters', monsters);
+app.use('/habitats', habitats);
 
 app.use((err, req, res, next) => { // next to send data around in middlewares
 	res.json(err);
