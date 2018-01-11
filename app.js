4cd42e60bd6197de16642f14e78bfc38d3f27a27
allen-fang <allenfang08@gmail.com>
2018-01-11 12:42:29 -0800

    added relationships table

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const monsters = require('./routes/monsters');
-const habitats = require('./routes/habitats');
+const routes = require('./routes');
 
 const app = express();
 
-
 // middlewares
 app.use(bodyParser.json());
-app.use('/monsters', monsters);
-app.use('/habitats', habitats);
+app.use('/', routes);
 
 app.use((err, req, res, next) => { // next to send data around in middlewares
 	res.json(err);
